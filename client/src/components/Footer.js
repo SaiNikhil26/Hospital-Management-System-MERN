cdf1abe778a3afeb641ea68c760758ff322b69a8
Anush <anushiyer10@gmail.com>
2023-11-25 15:45:48 +0530

    change

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => {
             <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
                 <div>
                     <img src={logo} alt="" />
-                    <p className='text-[16px] leading-7 font-[400] text-black mt-4'>Copyright © {year} Developed by Anush Iyer all rights reserved.</p>
+                    <p className='text-[16px] leading-7 font-[400] text-black mt-4'>Copyright © {year} Developed by Anush Iyer and Sai Nikhil all rights reserved.</p>
                     <div className='flex items-center gap-3 mt-4'>
                         {socialLinks.map((link,index)=><Link to={link.path} key={index} className='w-9 h-9 border border-solid border-[#181A1E]
                         rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none'>{link.icon}</Link>)}
